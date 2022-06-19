408ea5ab29d79b74ee7c8ae71089bb9a93851f2f
Noam Nol <noamnol19@gmail.com>
2022-06-20 00:09:35 +0300

    test: add pytest and test baseurl

---
+++ test/unittests/test_strapi_client_sync.py
@@ -0,0 +1,11 @@
+from pystrapi.strapi_client_sync import StrapiClientSync
+
+
+def test_baseurl() -> None:
+    client = StrapiClientSync('url/')
+    assert client.baseurl == 'url/'
+
+
+def test_baseurl_no_slash() -> None:
+    client = StrapiClientSync('url')
+    assert client.baseurl == 'url/'
