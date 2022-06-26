6a951237eaad00fcb569015dddccee6c30dec1ee
Noam Nol <noamnol19@gmail.com>
2022-06-26 14:51:36 +0300

    test: rename asyncutils.value to asvalue

--- test/integration/test_strapi_client.py
+++ test/integration/test_strapi_client.py
@@ -2,17 +2,17 @@ import pytest
 from pydash.predicates import is_match  # type: ignore
 
 from test.types import AnyStrapiClient
-from test.utils import asyncutils
+from test.utils.asyncutils import asvalue
 from pystrapi.types import StrapiEntriesResponse, StrapiEntryResponse
 
 
 async def test_get_entry(client: AnyStrapiClient, post1: dict) -> None:
-    res: StrapiEntryResponse = await asyncutils.value(client.get_entry('posts', 1))
+    res: StrapiEntryResponse = await asvalue(client.get_entry('posts', 1))
     assert is_match(res['data'], post1)
 
 
 async def test_get_entries(client: AnyStrapiClient, post1: dict, post2: dict) -> None:
-    res: StrapiEntriesResponse = await asyncutils.value(client.get_entries('posts'))
+    res: StrapiEntriesResponse = await asvalue(client.get_entries('posts'))
     assert res['data']
     assert is_match(res['data'][0], post1)
     assert is_match(res['data'][1], post2)
@@ -21,16 +21,16 @@ async def test_get_entries(client: AnyStrapiClient, post1: dict, post2: dict) ->
 async def test_create_and_delete_entry(auth_client: AnyStrapiClient, post3_attrs: dict) -> None:
     # TODO: split into smaller functions when db reset between tests is implemented
     # First, delete bad leftovers
-    find_res: StrapiEntriesResponse = await asyncutils.value(
+    find_res: StrapiEntriesResponse = await asvalue(
         auth_client.get_entries('posts', filters={"title": {"$eq": post3_attrs["title"]}}))
     if find_res['data']:
         # Delete if post already exists
-        await asyncutils.value(auth_client.delete_entry('posts', find_res['data'][0]['id']))
+        await asvalue(auth_client.delete_entry('posts', find_res['data'][0]['id']))
 
-    create_res: StrapiEntryResponse = await asyncutils.value(auth_client.create_entry('posts', post3_attrs))
+    create_res: StrapiEntryResponse = await asvalue(auth_client.create_entry('posts', post3_attrs))
     assert create_res['data']
     assert is_match(create_res['data']['attributes'], post3_attrs)
-    delete_res: StrapiEntryResponse = await asyncutils.value(
+    delete_res: StrapiEntryResponse = await asvalue(
         auth_client.delete_entry('posts', create_res['data']['id']))
     assert delete_res['data']
     assert is_match(delete_res['data']['attributes'], post3_attrs)
@@ -39,4 +39,4 @@ async def test_create_and_delete_entry(auth_client: AnyStrapiClient, post3_attrs
 async def test_delete_entry_forbidden(client: AnyStrapiClient) -> None:
     """Can't delete if client isn't authorized"""
     with pytest.raises(Exception):
-        await asyncutils.value(client.delete_entry('posts', 2))
+        await asvalue(client.delete_entry('posts', 2))
--- test/utils/asyncutils.py
+++ test/utils/asyncutils.py
@@ -5,7 +5,7 @@ from typing import Any, Coroutine, TypeVar, Union
 V = TypeVar("V", bound="Any")
 
 
-async def value(val: Union[V, Coroutine[Any, Any, V]]) -> V:
+async def asvalue(val: Union[V, Coroutine[Any, Any, V]]) -> V:
     """
     Return the given value. await if it's awaitable.
     """
