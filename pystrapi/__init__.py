b6d5e5868ef33da8350b4a1317e142e20b1b093d
Noam Nol <noamnol19@gmail.com>
2022-06-27 20:05:02 +0300

    feat: export connectors and enums

--- pystrapi/__init__.py
+++ pystrapi/__init__.py
@@ -1,4 +1,13 @@
+from .connector import Connector
+from .connector_sync import ConnectorSync
+from .helpers import process_data, process_response
+from .parameters import Filter, PublicationState
 from .strapi_client import StrapiClient
 from .strapi_client_sync import StrapiClientSync
-from .helpers import process_response, process_data
-__all__ = ['StrapiClient', 'StrapiClientSync', 'process_response', 'process_data']
+
+__all__ = [
+    'StrapiClient', 'StrapiClientSync',
+    'ConnectorSync', 'Connector',
+    'Filter', 'PublicationState',
+    'process_response', 'process_data'
+]
