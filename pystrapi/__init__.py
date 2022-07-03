fc22b184585fe94ec1805ed7e8b70d1e4c5181c8
Noam Nol <noamnol19@gmail.com>
2022-07-03 21:09:07 +0300

    feat: don't export helpers from __init__

--- pystrapi/__init__.py
+++ pystrapi/__init__.py
@@ -1,6 +1,5 @@
 from .connector import Connector
 from .connector_sync import ConnectorSync
-from .helpers import process_data, process_response
 from .parameters import Filter, PublicationState
 from .strapi_client import StrapiClient
 from .strapi_client_sync import StrapiClientSync
@@ -9,5 +8,4 @@ __all__ = [
     'StrapiClient', 'StrapiClientSync',
     'ConnectorSync', 'Connector',
     'Filter', 'PublicationState',
-    'process_response', 'process_data'
 ]
